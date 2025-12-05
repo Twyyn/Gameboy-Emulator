722611d5b283a349b42335c0dc8ff59713cf4e3b
Twyyn <131999553+Twyyn@users.noreply.github.com>
2025-12-05 23:31:19 +0000

    ...

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -1,13 +1,2 @@
 use crate::cpu::opcodes::{Mnemonic, MnemonicCB};
 
-pub struct Instruction {
-    opcode: Mnemonic,
-}
-
-impl Instruction {
-    pub fn decode(&self, opcode: Mnemonic) {
-        match opcode {
-            
-        }
-    }
-}
